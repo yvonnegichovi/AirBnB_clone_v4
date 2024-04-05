7959d660d89a9c69742fff5c6ff967c06c69ff8f
yvonnegichovi <yvonnegichovi@gmail.com>
2024-04-06 00:43:43 +0300

    task 1 and 2

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,19 +1,19 @@
 $(() => {
-    $('input[type="checkbox"]').change(function() {
-        const checkedAmenities = [];
-        $('input[type="checkbox"]:checked').each(function() {
-            checkedAmenities.push($(this).data('id'));
-        });
-        $('.amenities h4').text(checkedAmenities.join(', '));
+  $('input[type="checkbox"]').change(function () {
+    const checkedAmenities = [];
+    $('input[type="checkbox"]:checked').each(function () {
+      checkedAmenities.push($(this).data('id'));
     });
-$.get({
-    url: "http://0.0.0.0:5001/api/v1/status/",
+    $('.amenities h4').text(checkedAmenities.join(', '));
+  });
+  $.get({
+    url: 'http://0.0.0.0:5001/api/v1/status/',
     success: function (data) {
-        if (data.status = "OK") {
-            $("div#api_status").addClass("available");
-        } else {
-            $("div#api_status").removeClass("available");
-        }
+      if (data.status = 'OK') {
+        $('div#api_status').addClass('available');
+      } else {
+        $('div#api_status').removeClass('available');
+      }
     }
-    });
+  });
 });
