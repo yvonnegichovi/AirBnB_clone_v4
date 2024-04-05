2bcdea534b029f341d5f8826d1a1e5dc11b31f0f
yvonnegichovi <yvonnegichovi@gmail.com>
2024-04-05 13:34:39 +0300

    task 2 - fixing the storage of the checked boxes

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
     $('input[type="checkbox"]').change(function() {
         const checkedAmenities = [];
         $('input[type="checkbox"]:checked').each(function() {
-            checkedAmenities.push($(this).data('id'));
+            const amenityName = $(this).siblings('label').text().trim();
+            checkedAmenities.push(amenityName);
         });
         $('.amenities h4').text(checkedAmenities.join(', '));
     });
